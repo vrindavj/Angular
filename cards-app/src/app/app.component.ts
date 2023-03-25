52214c8592530e347daa2128542410a4b4ea2361
vrinda vj <vrindavdas@gmail.com>
2023-03-25 19:46:37 +0200

    child-parent comm

--- cards-app/src/app/app.component.ts
+++ cards-app/src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards-app';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of snowy mountain',
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'bikingWiz',
+      content: 'Biking adventure done right',
+    },
+  ];
 }
---
+++ cards-app/src/app/card/card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+export class CardComponent {
+  @Input() post = {
+    title: '',
+    imageUrl: '',
+    username: '',
+    content: '',
+  };
+}
